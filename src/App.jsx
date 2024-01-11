681087617585e2d39bc4b6829763dc817f687e2f
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-01-11 08:57:01 -0600

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App() {
   return (
     <Layout>
     <div className="App w-full text-black font-[300] h-fit text-[1.25rem]">
-      <section className='flex box-border flex-col gap-6 px-4 md:px-18 py-12 pt-4 md:grid md:grid-cols-2 md:gap-y-10 md:gap-x-12 md:p-12 md:grid-rows-[10%_80%] h-fit'>
+      <section className='flex box-border flex-col gap-6 px-4 md:px-10 xl:px-32 py-12 pt-4 md:grid md:grid-cols-2 md:gap-y-10 md:gap-x-12 md:p-12 md:grid-rows-[10%_80%] h-fit'>
           <h1 className='text-[2.5rem] h-fit'>Lucky 13</h1>
-          <p className='md:col-start-1 md:max-w-[500px]'>Lucky 13 Rescue is an organization dedicated to the rescue and aid of homeless
+          <p className='md:col-start-1 xl:justify-self-end'>Lucky 13 Rescue is an organization dedicated to the rescue and aid of homeless
             and neglected dogs in the Kansas City metro and surrounding rural areas. 
             We do not limit our efforts to a specific breed, but rather strive to assist 
             any dog that is facing neglect, homelessness, abuse or death. Our ultimate goal 
@@ -19,9 +19,9 @@ function App() {
             We are a foster based rescue as we do not have a facility to intake dogs on the spot.</p>
           <img className='w-full md:col-start-2 md:row-start-2 md:row-span-2 md:w-auto md:h-[90%] md:mx-auto object-cover object-[25%]' src="./troy.png" alt="" />
       </section>
-      <section className='bg-gradient-to-b from-blue to-lightblue text-white px-6 py-12 text-center flex flex-col items-center gap-6'>
+      <section className='bg-gradient-to-b from-blue to-lightblue text-white px-6 py-12 text-center flex flex-col items-center gap-6 md:gap-12'>
         <h2 className='text-[2rem]'>Foster</h2>
-        <p>
+        <p className='md: max-w-[500px]'>
         Save a life! Foster a homeless dog. 
         We provide you with all of the supplies and cover all vetting expenses.
         As a foster home based rescue, we are dependent upon local foster homes providing 
@@ -30,7 +30,7 @@ function App() {
         </p>
         <ApplyButton/>
       </section>
-      <section className='flex flex-col gap-6 px-6 py-12'>
+      <section className='flex flex-col gap-6 px-6 py-12 md:px-10 xl:px-32 py-12'>
       <h2 className='text-[2rem]'>Adoption Application</h2>
         <ol>
         <li>1. Fill out application</li>
@@ -40,7 +40,7 @@ function App() {
         </ol>
         <AdoptButton />
       </section>
-      <section className='bg-gradient-to-b from-blue to-lightblue text-white px-6 py-12 flex flex-col gap-6'>
+      <section className='bg-gradient-to-b from-blue to-lightblue md:px-10 xl:px-32 py-12 text-white px-6 py-12 flex flex-col gap-6'>
       <h2 className='text-[2rem]'>Donate</h2>
         <p>
         There are numerous ways you can donate to Lucky 13 including:
@@ -55,7 +55,7 @@ function App() {
       </section>
       <section className='text-center px-6 py-12 flex flex-col gap-6'>
         <h2 className='text-[2rem]'>Sponsors</h2>
-        <div className='grid grid-cols-2 grid-rows-3 items-center justify-items-center gap-4 p-2'>
+        <div className='grid grid-cols-2 grid-rows-3 items-center justify-items-center gap-4 p-2 md:flex flex-row justify-around md:p-20'>
           <img src="./sponsor-1.png" alt="" />
           <img src="./sponsor-2.png" alt="" />
           <img src="./sponsor-3.png" alt="" />
