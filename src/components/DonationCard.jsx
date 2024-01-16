d9cb1be1bb3aaaf370afb74419edeb8292e63d25
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-01-16 10:22:00 -0600

    added animation to donation cards

--- src/components/DonationCard.jsx
+++ src/components/DonationCard.jsx
@@ -1,5 +1,5 @@
+import { motion } from "framer-motion";
 import DonateButton from "./DonateButton";
-
 
 export default function DonationCard(props){
     const title = props.title;
@@ -7,7 +7,12 @@ export default function DonationCard(props){
     const donateButton = props.donateButton;
 
     return (
-        <section className={`${donateButton ? 'bg-blue' : 'bg-yellow'} justify-self-center h-[300px] min-h-fit md:w-[300px] md:h-[350px] w-[90%] rounded-xl border-black border-2`}>
+        <motion.section
+        whileInView={{ rotate: [-5, 0] }}
+        whileHover={{scale: 1.07}}
+        initial={{ rotate: 0, scale: 1}}
+        viewport={{ once: true }}
+         className={`${donateButton ? 'bg-blue' : 'bg-yellow'} justify-self-center h-[300px] min-h-fit md:w-[300px] md:h-[350px] w-[90%] rounded-xl border-black border-2`}>
             <div className='bg-white h-[95%] text-center p-4 flex flex-col justify-around items-center'>
                 <h3 className='text-[2rem]'>{title}</h3>
                 <p className='max-w-[80%] mx-auto'>{text}</p>
@@ -15,6 +20,6 @@ export default function DonationCard(props){
                     <DonateButton/>
                 )}                
             </div>
-        </section>
+        </motion.section>
     )
 }
