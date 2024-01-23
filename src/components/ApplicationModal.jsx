ecdaad69de99b3b382885ce6fe44bd40944451bf
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-01-23 12:06:07 -0600

    styling modal

--- src/components/ApplicationModal.jsx
+++ src/components/ApplicationModal.jsx
@@ -3,13 +3,27 @@ export default function ApplicationModal(props){
     return(
         <section className='text-white'>
             <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
-            <div className="modal-box">
-                <h3 className="font-bold text-lg">{dog}</h3>
-                <p className="py-4">Press ESC key or click the button below to close</p>
-                <div className="modal-action">
-                <form method="dialog">
-                    {/* if there is a button in form, it will close the modal */}
-                    <button className="btn">Close</button>
+            <div className="modal-box bg-blue h-fit w-full flex flex-col gap-4">
+                <h3 className="font-bold text-lg">Adopt {dog}</h3>
+                <div className='flex flex-col gap-4 w-full'>
+                    <input placeholder='Full Name' className='bg-white text-black' type="text" />
+                    <input placeholder='Email' className='bg-white text-black' type="text" />
+                    <select placeholder='Dog' className='bg-white text-black' type="text" >
+                    <option selected >{dog}</option>
+                    <option value="option 1">option 1</option>
+                    <option value="option 1">option 2</option>
+                    <option value="option 1">option 3</option>
+                    <option value="option 1">option 4</option>
+                    </select>
+                    <input placeholder='Street Address' className='bg-white text-black' type="text" />
+                    <input placeholder='City' className='bg-white text-black' type="text" />
+                    <input placeholder='Postal Code' className='bg-white text-black' type="text" />
+                    <input placeholder='Phone Number 1' className='bg-white text-black' type="text" />
+                    <input placeholder='Phone Number 2' className='bg-white text-black' type="text" />
+                </div>
+                <div className="modal-action justify-start w-full">
+                <form method="dialog w-full flex flex-col gap-4">
+                    <button className="text-white">Close</button>
                 </form>
                 </div>
             </div>
