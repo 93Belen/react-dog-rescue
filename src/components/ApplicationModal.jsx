dfd75bd73035425a6ecdc5bfcd817ec608415b57
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-01-23 12:17:35 -0600

    styling application form

--- src/components/ApplicationModal.jsx
+++ src/components/ApplicationModal.jsx
@@ -5,21 +5,22 @@ export default function ApplicationModal(props){
             <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
             <div className="modal-box bg-blue h-fit w-full flex flex-col gap-4">
                 <h3 className="font-bold text-lg">Adopt {dog}</h3>
-                <div className='flex flex-col gap-4 w-full'>
-                    <input placeholder='Full Name' className='bg-white text-black' type="text" />
-                    <input placeholder='Email' className='bg-white text-black' type="text" />
-                    <select placeholder='Dog' className='bg-white text-black' type="text" >
+                <div className='flex flex-col gap-4 w-full p-5'>
+                    <input placeholder='Full Name' className='bg-white text-black p-2 rounded-md' type="text" />
+                    <input placeholder='Email' className='bg-white text-black p-2 rounded-md' type="email" />
+                    <select placeholder='Dog' className='bg-white text-black p-2 rounded-md appearance-none' >
                     <option selected >{dog}</option>
                     <option value="option 1">option 1</option>
                     <option value="option 1">option 2</option>
                     <option value="option 1">option 3</option>
                     <option value="option 1">option 4</option>
                     </select>
-                    <input placeholder='Street Address' className='bg-white text-black' type="text" />
-                    <input placeholder='City' className='bg-white text-black' type="text" />
-                    <input placeholder='Postal Code' className='bg-white text-black' type="text" />
-                    <input placeholder='Phone Number 1' className='bg-white text-black' type="text" />
-                    <input placeholder='Phone Number 2' className='bg-white text-black' type="text" />
+                    <input placeholder='Street Address' className='bg-white text-black p-2 rounded-md' type="text" />
+                    <input placeholder='City' className='bg-white text-black p-2 rounded-md' type="text" />
+                    <input placeholder='Postal Code' className='bg-white text-black p-2 rounded-md' type="text" />
+                    <input placeholder='Phone Number 1' className='bg-white text-black p-2 rounded-md' type="tel" />
+                    <input placeholder='Phone Number 2' className='bg-white text-black p-2 rounded-md' type="tel" />
+                    <button>Submit</button>
                 </div>
                 <div className="modal-action justify-start w-full">
                 <form method="dialog w-full flex flex-col gap-4">
