8a3c1d39a2e662fd8868139f9cbcd18dcf398f31
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-01-25 14:05:27 -0600

    added carousel

--- src/components/DogModal.jsx
+++ src/components/DogModal.jsx
@@ -5,22 +5,26 @@ export default function DogModal(props){
         <dialog id="my_modal_dog" className="modal modal-bottom sm:modal-middle">
         <div className="modal-box bg-blue h-fit w-full flex flex-col gap-4">
             <h3 className="font-bold text-lg">{dog}</h3>
-            <form className='flex flex-col gap-4 w-full p-5'>
-                <input required placeholder='Full Name' className='bg-white text-black p-2 rounded-md' type="text" />
-                <input required placeholder='Email' className='bg-white text-black p-2 rounded-md' type="email" />
-                <select required placeholder='Dog' className='bg-white text-black p-2 rounded-md appearance-none' >
-                <option value="option 1">option 1</option>
-                <option value="option 1">option 2</option>
-                <option value="option 1">option 3</option>
-                <option value="option 1">option 4</option>
-                </select>
-                <input required placeholder='Street Address' className='bg-white text-black p-2 rounded-md' type="text" />
-                <input required placeholder='City' className='bg-white text-black p-2 rounded-md' type="text" />
-                <input required placeholder='Postal Code' className='bg-white text-black p-2 rounded-md' type="text" />
-                <input required placeholder='Phone Number 1' className='bg-white text-black p-2 rounded-md' type="tel" />
-                <input placeholder='Phone Number 2' className='bg-white text-black p-2 rounded-md' type="tel" />
-                <button type='submit'>Submit</button>
-            </form>
+            <div className="carousel w-full">
+                <div id="item1" className="carousel-item w-full">
+                    <img src="./troy.png" className="w-full" />
+                </div> 
+                <div id="item2" className="carousel-item w-full">
+                    <img src="./troy-4.png" className="w-full" />
+                </div> 
+                <div id="item3" className="carousel-item w-full">
+                    <img src="./troy-2.png" className="w-full" />
+                </div> 
+                <div id="item4" className="carousel-item w-full">
+                    <img src="./troy-3.png" className="w-full" />
+                </div>
+                </div> 
+                <div className="flex justify-center w-full py-2 gap-2">
+                <a href="#item1" className="btn btn-xs">1</a> 
+                <a href="#item2" className="btn btn-xs">2</a> 
+                <a href="#item3" className="btn btn-xs">3</a> 
+                <a href="#item4" className="btn btn-xs">4</a>
+                </div>
             <div className="modal-action justify-start w-full">
             <form method="dialog w-full flex flex-col gap-4">
                 <button className="text-white">Close</button>
