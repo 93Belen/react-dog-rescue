9b1289918201faa5f67c7625b1506fbc23da8b9b
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-01-30 05:21:09 -0600

    change modal width

--- src/components/DogModal.jsx
+++ src/components/DogModal.jsx
@@ -1,11 +1,17 @@
+import Chip from "./Chip"
+
 export default function DogModal(props){
     const dog = props.dog
+    const sex = props.sex
+    const age = props.age
+    const breed = props.breed
+    const size = props.size
     return(
         <section className='text-white'>
-        <dialog id="my_modal_dog" className="modal modal-bottom sm:modal-middle">
-        <div className="modal-box bg-blue h-fit w-full flex flex-col gap-4">
+        <dialog id="my_modal_dog" className="modal">
+        <div className="modal-box w-screen md:w-[70vw] max-w-[90vw] bg-blue h-fit flex flex-col gap-4">
             <h3 className="font-bold text-lg">{dog}</h3>
-            <div className="carousel w-full">
+            <div className="carousel w-full md:w-[400px] m-auto">
                 <div id="item1" className="carousel-item w-full">
                     <img src="./troy.png" className="w-full" />
                 </div> 
@@ -24,6 +30,15 @@ export default function DogModal(props){
                 <a href="#item2" className="btn btn-xs">2</a> 
                 <a href="#item3" className="btn btn-xs">3</a> 
                 <a href="#item4" className="btn btn-xs">4</a>
+                </div>
+                <div className='flex justify-around md:w-[50%]'>
+                    <Chip text={sex} />
+                    <Chip text={size} />
+                    <Chip text={breed} />
+                    <Chip text={age} />
+                </div>
+                <div>
+                    
                 </div>
                 <div className="modal-action justify-start w-full">
                 <form method="dialog w-full flex flex-col gap-4">
