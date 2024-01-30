56011bdc6bf05871f0c3c0aac2f82aa195cf2fdc
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-01-30 05:45:15 -0600

    styling pic in modal

--- src/components/DogModal.jsx
+++ src/components/DogModal.jsx
@@ -7,11 +7,12 @@ export default function DogModal(props){
     const breed = props.breed
     const size = props.size
     return(
-        <section className='text-black'>
+        <section className='text-white'>
         <dialog id="my_modal_dog" className="modal">
-        <div className="modal-box w-screen md:w-[70vw] max-w-[90vw] bg-white h-full flex flex-col gap-4 md:p-12 md:gap-12">
+        <div className="modal-box w-screen md:w-[70vw] max-w-[90vw] bg-black h-full flex flex-col justify-between md:p-12 ">
             <h3 className="font-bold text-[2rem]">{dog}</h3>
-            <div className="carousel w-full md:w-[400px] m-auto">
+            <div className='mx-auto'>
+            <div className="carousel w-auto md:w-[400px] m-auto max-h-[35vh]">
                 <div id="item1" className="carousel-item w-full">
                     <img src="./troy.png" className="w-full" />
                 </div> 
@@ -26,11 +27,13 @@ export default function DogModal(props){
                 </div>
                 </div> 
                 <div className="flex justify-center w-full py-2 gap-2">
-                <a href="#item1" className="btn btn-xs">1</a> 
-                <a href="#item2" className="btn btn-xs">2</a> 
-                <a href="#item3" className="btn btn-xs">3</a> 
-                <a href="#item4" className="btn btn-xs">4</a>
+                <a href="#item1" className="btn bg-white text-black btn-xs">1</a> 
+                <a href="#item2" className="btn bg-white text-black btn-xs">2</a> 
+                <a href="#item3" className="btn bg-white text-black btn-xs">3</a> 
+                <a href="#item4" className="btn bg-white text-black btn-xs">4</a>
                 </div>
+            </div>
+            
                 <div className='flex justify-between md:w-[75%]'>
                     <Chip text={sex} />
                     <Chip text={size} />
