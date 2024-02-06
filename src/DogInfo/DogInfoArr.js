63b30acacf31b31d4cb82d1dddee17a5389c9762
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-02-06 08:32:50 -0600

    added dogs info array

---
+++ src/DogInfo/DogInfoArr.js
@@ -0,0 +1,44 @@
+export const DogInfoArr = [
+    {
+        sex: 'Female',
+        age: 'Puppy',
+        Breed: 'Pit Mix',
+        size: 'Small'
+    },
+    {
+        sex: 'Male',
+        age: 'Puppy',
+        Breed: 'Chihuahua',
+        size: 'Small'
+    },
+    {
+        sex: 'Female',
+        age: 'Young',
+        Breed: 'Lab Mix',
+        size: 'Small'
+    },
+    {
+        sex: 'Male',
+        age: 'Young',
+        Breed: 'Pit Mix',
+        size: 'Small'
+    },
+    {
+        sex: 'Female',
+        age: 'Adult',
+        Breed: 'Lab Mix',
+        size: 'Small'
+    },
+    {
+        sex: 'Male',
+        age: 'Adult',
+        Breed: 'Rottweiler',
+        size: 'Small'
+    },
+    {
+        sex: 'Female',
+        age: 'Senior',
+        Breed: 'Pit Mix',
+        size: 'Small'
+    }
+]
