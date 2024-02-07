b912d98becddd762d12c0d935d87775cc72fed1c
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-02-07 07:02:22 -0600

    object logic for sex, sex and age, sex age and size, and sex and size

--- src/DogInfo/OrganizeDogInfo.js
+++ src/DogInfo/OrganizeDogInfo.js
@@ -7,25 +7,25 @@ export default function OrganizeDogInfo(arr){
             puppy: {
                 all: [], // SEX AND AGE CHECKED
                 small: [], // SEX AGE AND SIZE CHECKED
-                mediumd: [], 
-                big: [] 
+                medium: [], 
+                large: [] 
             },
             young: {
                 all: [], // SEX AND AGE CHECKED
                 small: [], // SEX AGE AND SIZE CHECKED
-                mediumd: [], 
+                medium: [], 
                 large: [] 
             },
             adult: {
                 all: [], // SEX AND AGE CHECKED
                 small: [], // SEX AGE AND SIZE CHECKED
-                mediumd: [], 
+                medium: [], 
                 large: [] 
             },
             senior: {
                 all: [], // SEX AND AGE CHECKED
                 small: [], // SEX AGE AND SIZE CHECKED
-                mediumd: [], 
+                medium: [], 
                 large: [] 
             },
             small: [], // SEX AND SIZE CHECKED
@@ -37,25 +37,25 @@ export default function OrganizeDogInfo(arr){
             puppy: {
                 all: [], // SEX AND AGE CHECKED
                 small: [], // SEX AGE AND SIZE CHECKED
-                mediumd: [],
-                big: []
+                medium: [],
+                large: []
             },
             young: {
                 all: [], // SEX AND AGE CHECKED
                 small: [], // SEX AGE AND SIZE CHECKED
-                mediumd: [],
+                medium: [],
                 large: []
             },
             adult: {
                 all: [], // SEX AND AGE CHECKED
                 small: [], // SEX AGE AND SIZE CHECKED
-                mediumd: [],
+                medium: [],
                 large: []
             },
             senior: {
                 all: [], // SEX AND AGE CHECKED
                 small: [], // SEX AGE AND SIZE CHECKED
-                mediumd: [],
+                medium: [],
                 large: []
             },
             small: [], // SEX AND SIZE CHECKED
@@ -109,9 +109,129 @@ export default function OrganizeDogInfo(arr){
         // CHECK SEX
         if(dog.sex === 'Female'){
             obj.female.all.push(dog)
+            // CHECK SEX AND AGE
+            if(dog.age === 'Puppy'){
+                obj.female.puppy.all.push(dog)
+                // CHECK SEX AGE AND SIZE
+                if(dog.size === 'Small'){
+                    obj.female.puppy.small.push(dog)
+                }
+                else if(dog.size === 'Medium'){
+                    obj.female.puppy.medium.push(dog)
+                }
+                else if(dog.size === 'Large'){
+                    obj.female.puppy.large.push(dog)
+                }
+            }
+            else if(dog.age === 'Young'){
+                obj.female.young.all.push(dog)
+                if(dog.size === 'Small'){
+                    obj.female.young.small.push(dog)
+                }
+                else if(dog.size === 'Medium'){
+                    obj.female.young.medium.push(dog)
+                }
+                else if(dog.size === 'Large'){
+                    obj.female.young.large.push(dog)
+                }
+            }
+            else if(dog.age === 'Adult'){
+                obj.female.adult.all.push(dog)
+                if(dog.size === 'Small'){
+                    obj.female.adult.small.push(dog)
+                }
+                else if(dog.size === 'Medium'){
+                    obj.female.adult.medium.push(dog)
+                }
+                else if(dog.size === 'Large'){
+                    obj.female.adult.large.push(dog)
+                }
+            }
+            else if(dog.age === 'Senior'){
+                obj.female.senior.all.push(dog)
+                if(dog.size === 'Small'){
+                    obj.female.senior.small.push(dog)
+                }
+                else if(dog.size === 'Medium'){
+                    obj.female.senior.medium.push(dog)
+                }
+                else if(dog.size === 'Large'){
+                    obj.female.senior.large.push(dog)
+                }
+            }
+            // CHECK SEX AND SIZE
+            if(dog.size === 'Small'){
+                obj.female.small.push(dog)
+            }
+            else if(dog.size === 'Medium'){
+                obj.female.medium.push(dog)
+            }
+            else if(dog.size === 'Large'){
+                obj.female.large.push(dog)
+            }
         }
         else if(dog.sex === 'Male'){
             obj.male.all.push(dog)
+            // CHECK SEX AND AGE
+            if(dog.age === 'Puppy'){
+                obj.male.puppy.all.push(dog)
+                // CHECK SEX AGE AND SIZE
+                if(dog.size === 'Small'){
+                    obj.male.puppy.small.push(dog)
+                }
+                else if(dog.size === 'Medium'){
+                    obj.male.puppy.medium.push(dog)
+                }
+                else if(dog.size === 'Large'){
+                    obj.male.puppy.large.push(dog)
+                }
+            }
+            else if(dog.age === 'Young'){
+                obj.male.young.all.push(dog)
+                if(dog.size === 'Small'){
+                    obj.male.young.small.push(dog)
+                }
+                else if(dog.size === 'Medium'){
+                    obj.male.young.medium.push(dog)
+                }
+                else if(dog.size === 'Large'){
+                    obj.male.young.large.push(dog)
+                }
+            }
+            else if(dog.age === 'Adult'){
+                obj.male.adult.all.push(dog)
+                if(dog.size === 'Small'){
+                    obj.male.adult.small.push(dog)
+                }
+                else if(dog.size === 'Medium'){
+                    obj.male.adult.medium.push(dog)
+                }
+                else if(dog.size === 'Large'){
+                    obj.male.adult.large.push(dog)
+                }
+            }
+            else if(dog.age === 'Senior'){
+                obj.male.senior.all.push(dog)
+                if(dog.size === 'Small'){
+                    obj.male.senior.small.push(dog)
+                }
+                else if(dog.size === 'Medium'){
+                    obj.male.senior.medium.push(dog)
+                }
+                else if(dog.size === 'Large'){
+                    obj.male.senior.large.push(dog)
+                }
+            }
+            // CHECK SEX AND SIZE
+            if(dog.size === 'Small'){
+                obj.male.small.push(dog)
+            }
+            else if(dog.size === 'Medium'){
+                obj.male.medium.push(dog)
+            }
+            else if(dog.size === 'Large'){
+                obj.male.large.push(dog)
+            }
         }
 
         // CHECK AGE
