cf86a5b8768dcdd47cd39e24c9f29f008ba483a6
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-02-14 09:46:58 -0600

    added date of arrival

--- src/DogInfo/DogInfoArr.js
+++ src/DogInfo/DogInfoArr.js
@@ -3,49 +3,57 @@ export const DogInfoArr = [
         sex: 'Female',
         age: 'Puppy',
         breed: 'Pit Mix',
-        size: 'Large'
+        size: 'Large',
+        arrived: new Date('2024-02-14')
     },
     {
         sex: 'Male',
         age: 'Puppy',
         breed: 'Chihuahua',
-        size: 'Small'
+        size: 'Small',
+        arrived: new Date('2024-02-14')
     },
     {
         sex: 'Female',
         age: 'Young',
         breed: 'Lab Mix',
-        size: 'Medium'
+        size: 'Medium',
+        arrived: new Date('2024-02-14')
     },
     {
         sex: 'Male',
         age: 'Young',
         breed: 'Pit Mix',
-        size: 'Medium'
+        size: 'Medium',
+        arrived: new Date('2024-02-14')
     },
     {
         sex: 'Female',
         age: 'Adult',
         breed: 'Lab Mix',
-        size: 'Medium'
+        size: 'Medium',
+        arrived: new Date('2024-02-14')
     },
     {
         sex: 'Male',
         age: 'Adult',
         breed: 'Rottwailer',
-        size: 'Large'
+        size: 'Large',
+        arrived: new Date('2024-02-14')
     },
     {
         sex: 'Female',
         age: 'Senior',
         breed: 'Pit Mix',
-        size: 'Medium'
+        size: 'Medium',
+        arrived: new Date('2024-02-14')
     },
     {
         sex: 'Female',
         age: 'Adult',
         breed: 'Chihuahua',
-        size: 'Small'
+        size: 'Small',
+        arrived: new Date('2024-02-14')
     },
     {
         sex: 'Female',
@@ -57,24 +65,28 @@ export const DogInfoArr = [
         sex: 'Male',
         age: 'Senior',
         breed: 'Lab Mix',
-        size: 'Small'
+        size: 'Small',
+        arrived: new Date('2024-02-14')
     },
     {
         sex: 'Female',
         age: 'Senior',
         breed: 'Rottwailer',
-        size: 'Large'
+        size: 'Large',
+        arrived: new Date('2024-02-14')
     },
     {
         sex: 'Female',
         age: 'Young',
         breed: 'Lab Mix',
-        size: 'Large'
+        size: 'Large',
+        arrived: new Date('2024-02-14')
     },
     {
         sex: 'Female',
         age: 'Young',
         breed: 'Lab Mix',
-        size: 'Small'
+        size: 'Small',
+        arrived: new Date('2024-02-14')
     }
 ]
