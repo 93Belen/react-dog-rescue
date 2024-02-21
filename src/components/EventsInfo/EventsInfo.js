11bfdb6cfe644e9ca6826a644744c37d4f6e2aa5
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-02-21 07:15:08 -0600

    added events info

---
+++ src/components/EventsInfo/EventsInfo.js
@@ -0,0 +1,32 @@
+export const eventsInfo = [
+    {
+        title: 'Puppy Yoga',
+        text: 'Come puppy pose with us and Lucky 13 Rescue! Add some playfulness to your practice while supporting a local canine organization.',
+        date: {
+            day: 17,
+            month: 'Dec',
+            time: '11:30am - 12:30pm'
+        },
+        location: '7245 Central, Kansas City, MO'
+    },
+    {
+        title: 'Cocktails and Cats',
+        text: 'Come puppy pose with us and Lucky 13 Rescue! Add some playfulness to your practice while supporting a local canine organization.',
+        date: {
+            day: 20,
+            month: 'Feb',
+            time: '05:30pm - 07:30pm'
+        },
+        location: '7245 Central, Kansas City, MO'
+    },
+    {
+        title: 'Open Doors',
+        text: 'Come puppy pose with us and Lucky 13 Rescue! Add some playfulness to your practice while supporting a local canine organization.',
+        date: {
+            day: 3,
+            month: 'March',
+            time: '11:00am - 03:00pm'
+        },
+        location: '7245 Central, Kansas City, MO'
+    }
+]
