9cd792f39bb64613ef67b95f7c593642a2ea7602
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-02-22 12:38:28 -0600

    donate hover

--- src/components/DonateButton.jsx
+++ src/components/DonateButton.jsx
@@ -5,13 +5,13 @@ export default function DonateButton(props){
 
     if(paypal){
         return (
-            <Link target="_blank" to='https://www.paypal.com/paypalme/lucky13rescue' className='bg-yellow rounded-2xl font-[200] w-[9.93rem] flex justify-center items-center h-[2.31rem] text-black text-[1.25rem]' >
+            <Link target="_blank" to='https://www.paypal.com/paypalme/lucky13rescue' className='bg-yellow rounded-2xl hover:bg-darkyellow duration-1000 font-[200] w-[9.93rem] flex justify-center items-center h-[2.31rem] text-black text-[1.25rem]' >
             Donate
             </Link>
         )
     } else {
         return (
-            <div onClick={()=>document.getElementById('my_modal_donation').showModal()} className='bg-yellow rounded-2xl font-[200] w-[9.93rem] flex justify-center items-center h-[2.31rem] text-black text-[1.25rem]'>
+            <div onClick={()=>document.getElementById('my_modal_donation').showModal()} className='bg-yellow hover:bg-darkyellow duration-1000 rounded-2xl font-[200] w-[9.93rem] flex justify-center items-center h-[2.31rem] text-black text-[1.25rem]'>
                 Donate
             </div>
         )
