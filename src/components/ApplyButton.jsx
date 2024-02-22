bad4b60b2a8cf61dbf6edb802644a59bf8caf1a5
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-02-22 12:37:00 -0600

    apply button hover

--- src/components/ApplyButton.jsx
+++ src/components/ApplyButton.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function ApplyButton(){
     return (
-        <Link className='bg-yellow rounded-2xl font-[200] w-[9.93rem] flex justify-center items-center h-[2.31rem] text-black text-[1.25rem]'>
+        <Link className='bg-yellow rounded-2xl font-[200] w-[9.93rem] hover:bg-darkyellow transition-1000 flex justify-center items-center h-[2.31rem] text-black text-[1.25rem]'>
             Apply
         </Link>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
       'blue':'#1D818B',
       'lightblue':'#46AEB9',
       'yellow':"#F1EB98",
+      'darkyellow': '#ECE36F',
       'black':'#313231',
       'white':'#F9F8F8',
       'gray': '#878787'
