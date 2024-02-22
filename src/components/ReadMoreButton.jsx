f24e58e65e3dd2a9ab21664da24a088e8e21607a
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-02-22 12:39:23 -0600

    all buttons hover state

--- src/components/ReadMoreButton.jsx
+++ src/components/ReadMoreButton.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export default function ReadMoreButton(){
     return (
-        <Link to='/dog-info' className='bg-yellow hidden md:flex rounded-2xl w-[9.93rem] font-[200] justify-center items-center h-[2.31rem] text-black text-[1.25rem]'>
+        <Link to='/dog-info' className='bg-yellow hidden md:flex hover:bg-darkyellow duration-1000 rounded-2xl w-[9.93rem] font-[200] justify-center items-center h-[2.31rem] text-black text-[1.25rem]'>
             Read More
         </Link>
     )
