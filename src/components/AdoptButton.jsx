0fd6f69def710ceb9877a2db5b1d5568b603c3ba
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-02-22 12:34:37 -0600

    hover adopt button

--- src/components/AdoptButton.jsx
+++ src/components/AdoptButton.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export default function AdoptButton(){
     return (
-        <Link onClick={()=>document.getElementById('my_modal_adoption').showModal()} className='bg-blue rounded-2xl w-[9.93rem] font-[200] flex justify-center items-center h-[2.31rem] text-white text-[1.25rem]'>
+        <Link onClick={()=>document.getElementById('my_modal_adoption').showModal()} className='bg-blue rounded-2xl w-[9.93rem] font-[200] flex justify-center items-center h-[2.31rem] text-white text-[1.25rem] hover:bg-lightblue duration-1000'>
             Adopt
         </Link>
     )
