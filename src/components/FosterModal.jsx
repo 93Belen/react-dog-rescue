f03b4a42386f1055f2393b1a40771d638f845a0b
Belen Garcia Martinez <belengm.dev@gmail.com>
2024-02-28 07:12:34 -0600

    styled modal for phone

--- src/components/FosterModal.jsx
+++ src/components/FosterModal.jsx
@@ -2,14 +2,14 @@ export default function FosterModal(){
     return(
         <section className='text-white'>
             <dialog id="my_modal_foster" className="modal modal-bottom sm:modal-middle">
-            <div className="modal-box bg-blue h-fit w-full flex flex-col gap-4">
+            <div className="modal-box bg-blue h-fit w-full flex flex-col gap-1 md:gap-4">
                 <h3 className="font-bold text-lg">Foster Application</h3>
-                <form className='flex flex-col gap-4 w-full p-5'>
+                <form className='flex flex-col gap-2 md:gap-4 w-full p-3 md:p-5'>
                     <input required placeholder='Full Name' className='bg-white text-black p-2 rounded-md' type="text" />
                     <input required placeholder='Email' className='bg-white text-black p-2 rounded-md' type="email" />
-                    <input required placeholder='Postal Code' className='bg-white text-black p-2 rounded-md' type="text" />
+                    <input required placeholder='Address' className='bg-white text-black p-2 rounded-md' type="text" />
                     <input required placeholder='Phone Number' className='bg-white text-black p-2 rounded-md' type="tel" />
-                    <textarea rows={7} placeholder='Tell us about you, your home, family, and preferences' className='bg-white text-black p-2 rounded-md' />
+                    <textarea rows={5} placeholder='Tell us about you, your home, and family' className='bg-white text-black p-2 rounded-md' />
                     <button type='submit'>Submit</button>
                 </form>
                 <div className="modal-action justify-start w-full">
